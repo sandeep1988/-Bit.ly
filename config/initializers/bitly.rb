d61b008d4fbe2f0894d594bd61b4fbfa57342d61
sandeep <schourey1988@gmail.com>
2015-06-02 16:49:17 +0530

    add

--- config/initializers/bitly.rb
+++ config/initializers/bitly.rb
@@ -1,5 +1,5 @@
 Bitly.configure do |config|
   config.api_version = 3
-  config.login = "USERNAME"
-  config.api_key = "API_KEY"
+  config.login = "o_2vk8n23eml"
+  config.api_key = "R_8df9853cec02411a88192f047b7aa000"
 end
